dcf04222d98f7df857ab511e877f498016530fb2
Yuri <Yuri.Chaves39@hotmail.com>
2022-05-09 22:22:16 -0300

    May-09-22

--- Js/index.js
+++ Js/index.js
@@ -14,5 +14,23 @@ listSelect.forEach(dino => {
         navOpen.classList.remove('ativo')
         const navToOpen = document.getElementById(idDinoSelected)
         navToOpen.classList.add('ativo')
+
+        const subSelect = document.querySelectorAll('.sub')
+        const infoDino = document.querySelectorAll('.i')
+        subSelect.forEach(info => {
+            info.addEventListener('click', () => {
+                const infoOpen = document.querySelector('.see')
+                infoOpen.classList.remove('see')
+                const idInfoSelected = info.attributes.id.value
+        
+                const infoToOpen = document.getElementById(idDinoSelected + '_info_' + idInfoSelected)
+                infoToOpen.classList.add('see')
+        
+                const navOpen = document.querySelector('.op')
+                navOpen.classList.remove('op')
+                const navToOpen = document.getElementById(idInfoSelected)
+                navToOpen.classList.add('op')
+            })
+        })
     })
 })
